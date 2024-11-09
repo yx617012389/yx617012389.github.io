4564848162a5c18504266ddbb25a1b5c03d0b6ba
yx617012389 <617012389@qq.com>
2024-11-10 04:42:50 +0800

    Site updated: 2024-11-10 04:42:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/My-New-Post/","2024/11/08/hello-world/"];function toRandomPost(){
+var posts=["2024/11/08/hello-world/","2024/11/08/Openwrt防检测—PC/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

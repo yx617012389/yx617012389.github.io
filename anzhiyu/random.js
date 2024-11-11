858d89fbebbd5260806d7951b961a24b8a5342dd
yx617012389 <617012389@qq.com>
2024-11-11 15:10:35 +0800

    Site updated: 2024-11-11 15:10:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/08/Openwrt防检测—PC/","2024/11/10/学习通刷课/","2024/11/08/hello-world/"];function toRandomPost(){
+var posts=["2024/11/10/学习通刷课/","2024/11/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
